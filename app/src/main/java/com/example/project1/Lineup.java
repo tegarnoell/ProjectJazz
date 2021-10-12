867c286b748bdbac9a5c2891f7efbe04294244c2
Noel <tegarnoel83@gmail.com>
2021-10-12 20:39:32 +0700

    the application

---
+++ app/src/main/java/com/example/project1/Lineup.java
@@ -0,0 +1,26 @@
+package com.example.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Lineup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lineup);
+    }
+
+    public void Jazz(View view) {
+        Intent intent = new Intent(Lineup.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void Getticket(View view) {
+        Intent intent = new Intent(Lineup.this, GetTicket.class);
+        startActivity(intent);
+    }
+}
