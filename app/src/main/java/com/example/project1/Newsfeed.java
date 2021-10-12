f927e93a1ef8402e8d603a463615425cab20367d
Noel <tegarnoel83@gmail.com>
2021-10-12 22:05:53 +0700

    the application

--- app/src/main/java/com/example/project1/Newsfeed.java
+++ app/src/main/java/com/example/project1/Newsfeed.java
@@ -2,7 +2,9 @@ package com.example.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Newsfeed extends AppCompatActivity {
 
@@ -11,4 +13,20 @@ public class Newsfeed extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newsfeed);
     }
+
+    public void Jazz(View view) {
+        Intent intent = new Intent(Newsfeed.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void Getticket(View view) {
+        Intent intent = new Intent(Newsfeed.this, GetTicket.class);
+        startActivity(intent);
+    }
+
+    public void Lineup(View view) {
+        Intent intent = new Intent(Newsfeed.this, Lineup.class);
+        startActivity(intent);
+    }
+
 }
